eb3357986ec50b96aa1cf9f5d77400237bd0a5e2
Gustavo Santos <gustavofsantos@outlook.com>
2019-05-05 22:05:14 -0300

    Updates

---
+++ precache-manifest.c2b7f798dd9b603a066aa04d298f9851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e200cf3410510e0202571e62ef89b942",
+    "url": "/react-ts-switch/index.html"
+  },
+  {
+    "revision": "8d5d7b022d0bda38cecb",
+    "url": "/react-ts-switch/static/css/main.cedb377b.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/react-ts-switch/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "8d5d7b022d0bda38cecb",
+    "url": "/react-ts-switch/static/js/main.f1a18124.chunk.js"
+  },
+  {
+    "revision": "753f0c0caeb94c465b0d",
+    "url": "/react-ts-switch/static/js/runtime~main.3effa550.js"
+  }
+]);
